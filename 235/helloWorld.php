cd0e73ed5d6bdc6b3a2368fb4fab372c1a4cc39c
jbn0619 <jbn5379@g.rit.edu>
2019-10-05 22:15:00 -0400

    Added in some stylings and images and some php files.

---
+++ 235/helloWorld.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Hello 3</title>	
+</head>
+
+
+<body>
+	<?php
+		echo "<h1>Hello World!</h1>";
+		echo "<p>It's " . date('r');
+	?>
+	
+	<?php
+		phpinfo();
+	?>
+
+</body>
+
+</html>
+
