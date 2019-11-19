d2657f8f0991a564aa8812ea10419c6b4ede0994
jbn0619 <jbn5379@g.rit.edu>
2019-11-18 23:25:04 -0500

    Began work on portfolio
    Began to add basic structure to the portfolio.

---
+++ 235/portfolio/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html langg="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="portfolio.css"/>
+        <title>Justin Neft's Portfolio</title>
+    </head>
+
+    <body>
+        <header>
+            <h1>Justin Neft</h1>
+            <nav>
+                <ul id="listItems">
+                    <li>Home</li>
+                    <li>Portfolio</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+        
+        <main>
+            <section id="intro">
+                <h3>Justin Neft</h3>
+                <p>
+
+                </p>
+            </section>
+    
+            <section id="portfolio">
+                <h3>Projects</h3>
+                <div class="project">
+
+                </div>
+            </section>
+        </main>
+
+        <footer>
+            <section id="about">
+                <h3>About</h3>
+                <p>
+
+                </p>
+            </section>
+    
+            <section id="contact">
+                <h3>Contact Me</h3>
+
+                <?php include "emailForm.php"; ?>
+                
+            </section>
+        </footer>   
+    </body>
+</html>
