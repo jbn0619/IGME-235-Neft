bed177e20112d7ded989811524a09c3c0e1ec45b
jbn0619 <jbn5379@g.rit.edu>
2019-12-15 13:44:33 -0500

    Removed  redundant files
    Removed an extra copy of the 235 folder that found its way into the repo.

--- 235/phpICE/php-ICE-1.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-	define("PI", 3.14159);
-	$radius = 10;
-    $area = PI * $radius * $radius;
-    $circumference = PI * 2 * $radius;
-   echo "<p>The area of a radius $radius circle is $area</p>";
-   echo "<p>The circumference of a circle with radius $radius is $circumference</p>";
-?>
--- 235/phpTesting/broken.php
+++
@@ -1,3 +0,0 @@
-<?php
-	echo "Welcome!!!!  // missing closing quote and semicolon
-?>
